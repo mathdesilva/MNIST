e5774f97b09056c904793f75ebfe36be45c7e414
Matheus de Andrade Silva <matheusdeandradesv@gmail.com>
2023-05-14 19:58:04 -0300

    Models training

--- models/model_class.py
+++ models/model_class.py
@@ -2,17 +2,20 @@ from torch import nn
 IMAGE_SIZE = 28 * 28
 class ModelClass(nn.Module):
     def __init__(self):
-        super().__init__()
-        self.flatten = nn.Flatten()
+        super(ModelClass, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(IMAGE_SIZE, 128),
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 3)),
             nn.ReLU(),
-            nn.Linear(128, 512),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3)),
             nn.ReLU(),
-            nn.Linear(512, 10),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3)),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(64*(28-6)*(28-6), 128),
+            nn.ReLU(),
+            nn.Linear(128, 10),
             nn.Softmax(dim=1)
         )
 
     def forward(self, x):
-        x = self.flatten(x)
         return self.model(x)
